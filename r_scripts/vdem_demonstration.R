fda8452a99645b69eb4c37e9f1c59efd01abd17d
bob_kubinec <rmk7xy@virginia.edu>
2016-07-12 17:29:00 +0200

    working on demonstration

--- r_scripts/vdem_demonstration.R
+++ r_scripts/vdem_demonstration.R
@@ -1,3 +1,6 @@
 # This is a demonstration of 2-way and one-way FX analysis using data provided by the varities of democracy
 # Robert Kubinec 7/12/2016
 
+require(haven)
+require(dplyr)
+require(magrittr)
